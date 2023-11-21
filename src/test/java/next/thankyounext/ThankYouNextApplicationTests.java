223b9fe4756c475177e7d87311ddbf56501d8720
Luke_YS <yjsmk0902@gmail.com>
2023-11-21 15:12:30 +0900

    Init: Init Spring Setting - 스프링 초기 설정 - README.md 구성

---
+++ src/test/java/next/thankyounext/ThankYouNextApplicationTests.java
@@ -0,0 +1,13 @@
+package next.thankyounext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThankYouNextApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
