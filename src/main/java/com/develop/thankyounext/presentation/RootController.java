b58f711920f9ae9e499b644dee831b2b6f61f162
Luke_YS <yjsmk0902@gmail.com>
2024-01-03 16:25:29 +0900

    :sparkles: Feat: AWS EBS Health Check 기능 추가

---
+++ src/main/java/com/develop/thankyounext/presentation/RootController.java
@@ -0,0 +1,13 @@
+package com.develop.thankyounext.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Good!";
+    }
+}
