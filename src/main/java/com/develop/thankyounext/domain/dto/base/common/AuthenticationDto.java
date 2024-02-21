d29ff1c0dcb6377d78bf84a01ee5aec3de5854fa
Luke_YS <yjsmk0902@gmail.com>
2024-02-21 18:08:03 +0900

    :sparkles: 인증 DTO 추가 및 Security 설정 (#27)

---
+++ src/main/java/com/develop/thankyounext/domain/dto/base/common/AuthenticationDto.java
@@ -0,0 +1,70 @@
+package com.develop.thankyounext.domain.dto.base.common;
+
+import com.develop.thankyounext.domain.enums.RoleType;
+import lombok.Builder;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public record AuthenticationDto(
+        Long id,
+        String username,
+        Collection<? extends GrantedAuthority> authorities
+) implements UserDetails {
+    @Override
+    public Long id() {
+        return id;
+    }
+
+    @Builder
+    public AuthenticationDto {
+        if (Objects.isNull(authorities)) {
+            authorities = Set.of(RoleType.GUEST).stream()
+                    .map(RoleType::getKey)
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toUnmodifiableSet());
+        }
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
+
+
